cfdcb98038b4b6ba8f2bf0e5eeda115af1a00daf
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-11 09:26:12 +0530

    Refactor SIMD check cmake

--- simdcheck.cmake
+++ simdcheck.cmake
@@ -50,24 +50,39 @@ set(SIMD_SSE_PROG "
     }
 ")
 
-# TODO: Change to use include(CheckCXXSourceRuns)
 include(CheckCXXSourceRuns)
+include(CheckCXXCompilerFlag)
 
 function(AddSIMDCompilerFlag Target)
     message(STATUS "Running SIMD checks...")
+
+    
     if(MSVC)
+        check_cxx_compiler_flag("/arch:SSE2" COMPILER_SUPPORTS_SSE)
+        
         set(CMAKE_REQUIRED_FLAGS "/arch:AVX512")
             check_cxx_source_runs("${SIMD_AVX512_PROG}" HAS_AVX512_SUPPORT)
         unset(CMAKE_REQUIRED_FLAGS)
-        set(CMAKE_REQUIRED_FLAGS "/arch:AVX2")
-            check_cxx_source_runs("${SIMD_AVX2_PROG}" HAS_AVX2_SUPPORT)
-        unset(CMAKE_REQUIRED_FLAGS)
-        set(CMAKE_REQUIRED_FLAGS "/arch:AVX")
-            check_cxx_source_runs("${SIMD_AVX_PROG}" HAS_AVX_SUPPORT)
-        unset(CMAKE_REQUIRED_FLAGS)
-        set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
-            check_cxx_source_runs("${SIMD_SSE_PROG}" HAS_SSE_SUPPORT)
-        unset(CMAKE_REQUIRED_FLAGS)
+
+        if (NOT(HAS_AVX512_SUPPORT))
+            set(CMAKE_REQUIRED_FLAGS "/arch:AVX2")
+                check_cxx_source_runs("${SIMD_AVX2_PROG}" HAS_AVX2_SUPPORT)
+            unset(CMAKE_REQUIRED_FLAGS)
+            if(NOT(HAS_AVX2_SUPPORT))
+                set(CMAKE_REQUIRED_FLAGS "/arch:AVX")
+                    check_cxx_source_runs("${SIMD_AVX_PROG}" HAS_AVX_SUPPORT)
+                unset(CMAKE_REQUIRED_FLAGS)
+                if(NOT(HAS_AVX_SUPPORT))
+                    if(COMPILER_SUPPORTS_SSE)
+                        set(CMAKE_REQUIRED_FLAGS "/arch:SSE2")
+                            check_cxx_source_runs("${SIMD_SSE_PROG}" HAS_SSE_SUPPORT)
+                        unset(CMAKE_REQUIRED_FLAGS)
+                    else()
+                        check_cxx_source_runs("${SIMD_SSE_PROG}" HAS_SSE_SUPPORT)
+                    endif()
+                endif()
+            endif()
+        endif()
 
         if(HAS_AVX512_SUPPORT)
             target_compile_options(
@@ -86,7 +101,7 @@ function(AddSIMDCompilerFlag Target)
             message(STATUS "Enabled AVX")
         elseif(HAS_SSE_SUPPORT)
             target_compile_options(
-                ${Target} PRIVATE "/arch:SSE"
+                ${Target} PRIVATE "/arch:SSE2"
             )
             message(STATUS "${CPU_NAME} supports SSE. Enabling SSE...")
         else()
@@ -96,15 +111,22 @@ function(AddSIMDCompilerFlag Target)
         set(CMAKE_REQUIRED_FLAGS "-mavx512f")
             check_cxx_source_runs("${SIMD_AVX512_PROG}" HAS_AVX512_SUPPORT)
         unset(CMAKE_REQUIRED_FLAGS)
-        set(CMAKE_REQUIRED_FLAGS "-mavx2")
-            check_cxx_source_runs("${SIMD_AVX2_PROG}" HAS_AVX2_SUPPORT)
-        unset(CMAKE_REQUIRED_FLAGS)
-        set(CMAKE_REQUIRED_FLAGS "-mavx")
-            check_cxx_source_runs("${SIMD_AVX_PROG}" HAS_AVX_SUPPORT)
-        unset(CMAKE_REQUIRED_FLAGS)
-        set(CMAKE_REQUIRED_FLAGS "-msse4.2")
-            check_cxx_source_runs("${SIMD_SSE_PROG}" HAS_SSE_SUPPORT)
-        unset(CMAKE_REQUIRED_FLAGS)
+
+        if (NOT(HAS_AVX512_SUPPORT))
+            set(CMAKE_REQUIRED_FLAGS "-mavx2")
+                check_cxx_source_runs("${SIMD_AVX2_PROG}" HAS_AVX2_SUPPORT)
+            unset(CMAKE_REQUIRED_FLAGS)
+            if(NOT(HAS_AVX2_SUPPORT))
+                set(CMAKE_REQUIRED_FLAGS "-mavx")
+                    check_cxx_source_runs("${SIMD_AVX_PROG}" HAS_AVX_SUPPORT)
+                unset(CMAKE_REQUIRED_FLAGS)
+                if(NOT(HAS_AVX_SUPPORT))
+                    set(CMAKE_REQUIRED_FLAGS "-msse4.2")
+                        check_cxx_source_runs("${SIMD_SSE_PROG}" HAS_SSE_SUPPORT)
+                    unset(CMAKE_REQUIRED_FLAGS)
+                endif()
+            endif()
+        endif()
 
         if(HAS_AVX512_SUPPORT)
             target_compile_options(
@@ -131,68 +153,3 @@ function(AddSIMDCompilerFlag Target)
         endif()
     endif(MSVC)
 endfunction(AddSIMDCompilerFlag)
-
-
-# include(CheckCXXCompilerFlag)
-
-# function (AddSIMDCompilerFlag Target)
-#     if(MSVC)
-#         check_cxx_compiler_flag("/arch:AVX" HAS_AVX_SUPPORT)
-#         check_cxx_compiler_flag("/arch:AVX2" HAS_AVX2_SUPPORT)
-#         check_cxx_compiler_flag("/arch:AVX512" HAS_AVX512_SUPPORT)
-
-#         if(HAS_AVX512_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "/arch:AVX512"
-#             )
-#             message(STATUS "Enabled AVX512")
-#         elseif(HAS_AVX2_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "/arch:AVX2"
-#             )
-#             message(STATUS "Enabled AVX2")
-#         elseif(HAS_AVX_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "/arch:AVX"
-#             )
-#             message(STATUS "Enabled AVX")
-#         elseif(HAS_SSE_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "/arch:SSE"
-#             )
-#             message(STATUS "Enabling SSE...")
-#         else()
-#             message(STATUS "CPU doesn't support SIMD instruction set")
-#         endif()
-#     else()
-#         check_cxx_compiler_flag("-msse4.1" HAS_SSE_SUPPORT)
-#         check_cxx_compiler_flag("-mavx" HAS_AVX_SUPPORT)
-#         check_cxx_compiler_flag("-mavx2" HAS_AVX2_SUPPORT)
-#         check_cxx_compiler_flag("-mavx512f" HAS_AVX512_SUPPORT)
-
-#         if(HAS_AVX512_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "-mavx512f"
-#             )
-#             message(STATUS "Enabled AVX512")
-#         elseif(HAS_AVX2_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "-mavx2"
-#             )
-#             message(STATUS "Enabled AVX2")
-#         elseif(HAS_AVX_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "-mavx"
-#             )
-#             message(STATUS "Enabled AVX")
-#         elseif(HAS_SSE_SUPPORT)
-#             target_compile_options(
-#                 ${Target} PRIVATE "-msse4.1"
-#             )
-#             message(STATUS "Enabling SSE...")
-#         else()
-#             message(STATUS "CPU doesn't support SIMD instruction set")
-#         endif()
-#     endif()
-# endfunction(AddSIMDCompilerFlag)
-
