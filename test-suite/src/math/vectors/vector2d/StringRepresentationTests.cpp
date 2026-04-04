f0f653a06d419ab26966559fc96af5731bd69d79
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-04 06:09:27 +0530

    test: add string representation tests for vec2d

--- test-suite/src/math/vectors/vector2d/StringRepresentationTests.cpp
+++ test-suite/src/math/vectors/vector2d/StringRepresentationTests.cpp
@@ -1,87 +1,87 @@
-///**
-// * @file StringRepresentationTests.cpp
-// * @author Alan Abraham P Kochumon
-// * @date Created on: April 04, 2026
-// *
-// * @brief Verifies @ref fgm::Vector3D string representation.
-// *
-// * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
-// */
-//
-//
-//#include "Vector3DTestSetup.h"
-//
-//#include <sstream>
-//
-//
-//
-///**************************************
-// *                                    *
-// *                SETUP               *
-// *                                    *
-// **************************************/
-//
-//class Vector3DStringRepresentationTests: public ::testing::Test
-//{
-//    /** @brief Switch to use full precision for testing. */
-//    void SetUp() override
-//    {
-//        fgm::Config::useFullPrecision = true;
-//    }
-//
-//    /** @brief Switch back to normal log precision. */
-//    void TearDown() override
-//    {
-//        fgm::Config::useFullPrecision = false;
-//    }
-//};
-//
-//
-//
-///**
-// * @addtogroup T_FGM_Vec3_String_Repr
-// * @{
-// */
-//
-///** @test Verify that the string representation matches the expected format for integral types. */
-//TEST_F(Vector3DStringRepresentationTests, IntegralRepresentation)
-//{
-//    constexpr fgm::Vector3D vec(1, 2, 3);
-//    std::stringstream ss;
-//
-//    ss << vec;
-//
-//    EXPECT_EQ(ss.str(), "<1, 2, 3>\n");
-//}
-//
-//
-///**
-// * @test Verify that the string representation matches the expected format for single-precision
-// *       floating-point types.
-// */
-//TEST_F(Vector3DStringRepresentationTests, FloatRepresentation)
-//{
-//    constexpr fgm::Vector3D vec(1.2345f, 2.0f, 3.56789f);
-//    std::stringstream ss;
-//
-//    ss << vec;
-//
-//    EXPECT_EQ(ss.str(), "<1.23450, 2.00000, 3.56789>\n");
-//}
-//
-//
-///**
-// * @test Verify that the string representation matches the expected format for double-precision
-// *       floating-point types.
-// */
-//TEST_F(Vector3DStringRepresentationTests, DoubleRepresentation)
-//{
-//    constexpr fgm::Vector3D vec(1.2345789777, 2.65831, 3.161285);
-//    std::stringstream ss;
-//
-//    ss << vec;
-//
-//    EXPECT_EQ(ss.str(), "<1.234578977700000, 2.658310000000000, 3.161285000000000>\n");
-//}
-//
-///** @} */
+/**
+ * @file StringRepresentationTests.cpp
+ * @author Alan Abraham P Kochumon
+ * @date Created on: April 04, 2026
+ *
+ * @brief Verifies @ref fgm::Vector2D string representation.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+
+#include "Vector2DTestSetup.h"
+
+#include <sstream>
+
+
+
+/**************************************
+ *                                    *
+ *                SETUP               *
+ *                                    *
+ **************************************/
+
+class Vector2DStringRepresentationTests: public ::testing::Test
+{
+    /** @brief Switch to use full precision for testing. */
+    void SetUp() override
+    {
+        fgm::Config::useFullPrecision = true;
+    }
+
+    /** @brief Switch back to normal log precision. */
+    void TearDown() override
+    {
+        fgm::Config::useFullPrecision = false;
+    }
+};
+
+
+
+/**
+ * @addtogroup T_FGM_Vec2_String_Repr
+ * @{
+ */
+
+/** @test Verify that the string representation matches the expected format for integral types. */
+TEST_F(Vector2DStringRepresentationTests, IntegralRepresentation)
+{
+    constexpr fgm::Vector2D vec(1, 2);
+    std::stringstream ss;
+
+    ss << vec;
+
+    EXPECT_EQ(ss.str(), "<1, 2>\n");
+}
+
+
+/**
+ * @test Verify that the string representation matches the expected format for single-precision
+ *       floating-point types.
+ */
+TEST_F(Vector2DStringRepresentationTests, FloatRepresentation)
+{
+    constexpr fgm::Vector2D vec(1.2345f, 2.0f);
+    std::stringstream ss;
+
+    ss << vec;
+
+    EXPECT_EQ(ss.str(), "<1.23450, 2.00000>\n");
+}
+
+
+/**
+ * @test Verify that the string representation matches the expected format for double-precision
+ *       floating-point types.
+ */
+TEST_F(Vector2DStringRepresentationTests, DoubleRepresentation)
+{
+    constexpr fgm::Vector2D vec(1.2345789777, 2.65831);
+    std::stringstream ss;
+
+    ss << vec;
+
+    EXPECT_EQ(ss.str(), "<1.234578977700000, 2.658310000000000>\n");
+}
+
+/** @} */
