d01c8e264b1262d51ee911e83b27d3fc6d348393
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-08 17:04:11 +0530

    feat: add SIMDMemory files

---
+++ simd/include/utils/SIMDUtils.h
@@ -0,0 +1,49 @@
+#pragma once
+/**
+ * @file SIMDUtils.h
+ * @author Alan Abraham P Kochumon
+ * @date Created on: March 07, 2026
+ *
+ * @brief SIMD utility functions.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+
+#include <bit>
+#include <cstddef>
+
+
+namespace falcon::simd
+{
+    struct PackingParams
+    {
+        std::size_t alignedByteSize;
+        std::size_t padding;
+        std::size_t packedRegisterWidth;
+        std::size_t registerCount;
+    };
+
+    /**
+     * @brief Calculate the aligned Byte Size, padding, packed register width, and number of registers required to store
+     *        the given bytes, with a given alignment.
+     *
+     * @param totalByteSize The total unaligned byte size.
+     * @param maxAlignAs    The maximum possible alignment for a given CPU architecture.
+     *                      **16 for SSE, 32 for AVX and AVX2, and 64 for AVX512**
+     *
+     * @return The aligned byte size, additional required padding, target register width,
+     *         and number of registers required to pack the given bytes.
+     */
+    constexpr PackingParams calculatePackedSize(const std::size_t totalByteSize, const std::size_t maxAlignAs)
+    {
+        if (totalByteSize < 16)
+            return PackingParams{ 16, 16 - totalByteSize, 16, 1 };
+
+        const std::size_t packedSize = std::bit_ceil(totalByteSize);
+        const std::size_t optimalRegisterWidth = std::min(packedSize, maxAlignAs);
+        const std::size_t numRegisters = packedSize / optimalRegisterWidth;
+
+        return PackingParams{ packedSize, packedSize - totalByteSize, optimalRegisterWidth, numRegisters };
+    }
+} // namespace falcon::simd
