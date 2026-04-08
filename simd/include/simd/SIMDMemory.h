3752d7924aa094d9c2a50dc1ef1db3b08ad0c25d
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-08 19:31:24 +0530

    docs: add file docs to SIMDMemory

---
+++ simd/include/simd/SIMDMemory.h
@@ -0,0 +1,17 @@
+#pragma once
+/**
+ * @file SIMDMemory.h
+ * @author Alan Abraham P Kochumon
+ * @date Created on: April 08, 2026
+ *
+ * @brief SIMD register loading and storing definitions.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+
+namespace falcon::simd
+{}
+
+
+#include "SIMDMemory.tpp"
---
+++ simd/include/simd/SIMDMemory.tpp
@@ -0,0 +1,11 @@
+#pragma once
+/**
+ * @file SIMDMemory.tpp
+ * @author Alan Abraham P Kochumon
+ * @date Created on: April 08, 2026
+ *
+ * @brief Function implementation for SIMD data loading to and from registers defined in `SIMDMemory.h`.
+ * 
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
