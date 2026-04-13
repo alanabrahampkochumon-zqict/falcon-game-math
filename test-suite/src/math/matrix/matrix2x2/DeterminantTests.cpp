062f0fd5437ea7a8db563f715657d6a61e3c3e4a
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-13 16:25:05 +0530

    test: add mat2d determinant static tests

--- test-suite/src/math/matrix/matrix2x2/DeterminantTests.cpp
+++ test-suite/src/math/matrix/matrix2x2/DeterminantTests.cpp
@@ -42,6 +42,7 @@ class SingularMatrix2D: public ::testing::TestWithParam<fgm::Matrix2D<float>>
 INSTANTIATE_TEST_SUITE_P(Matrix2DDeterminantTestSuite, SingularMatrix2D,
                          ::testing::Values(fgm::Matrix2D{ fgm::Vector2D{ 1, 2 }, fgm::Vector2D{ 1, 2 } },
                                            fgm::Matrix2D{ fgm::Vector2D{ 2, 2 }, fgm::Vector2D{2, 2} },
+                                           fgm::Matrix2D{ fgm::Vector2D{ 3, 2 }, fgm::Vector2D{6, 4} },
                                            fgm::Matrix2D{ fgm::Vector2D{ 0, 0 }, fgm::Vector2D{4, 5} },
                                            fgm::Matrix2D{ fgm::Vector2D{ 0, 3 }, fgm::Vector2D{0, 5} }
                                            ));
@@ -51,6 +52,32 @@ INSTANTIATE_TEST_SUITE_P(Matrix2DDeterminantTestSuite, SingularMatrix2D,
  * @addtogroup T_FGM_Mat2x2_Det
  * @{
  */
+
+/**************************************
+ *                                    *
+ *           STATIC TESTS             *
+ *                                    *
+ **************************************/
+
+/** @brief Verify that matrix determinant operation is available at compile time. */
+namespace 
+{
+    constexpr fgm::Matrix2D MATRIX{ fgm::Vector2D{ 4, 2 }, fgm::Vector2D{ 3, 4 } };
+    
+    // Determinant
+    static_assert(MATRIX.determinant() == 10);
+    static_assert(fgm::Matrix2D<int>::determinant(MATRIX) == 10);
+
+}
+
+
+
+/**************************************
+ *                                    *
+ *           RUNTIME TESTS            *
+ *                                    *
+ **************************************/
+
 
 /** @brief Verify that computing the determinant of a non-singular matrix returns a non-zero value. */
 TYPED_TEST(Matrix2DDeterminant, ReturnsNonZeroScalar)
