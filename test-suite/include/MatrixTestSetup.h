e0e0cfe619131dc7d27d61fd740b8e235fda3997
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-17 18:45:13 +0530

    test: add mat2d rotation factory test

--- test-suite/include/MatrixTestSetup.h
+++ test-suite/include/MatrixTestSetup.h
@@ -18,6 +18,7 @@
 #include <gtest/gtest.h>
 #include <limits>
 #include <matrix/Matrix2D.h>
+#include <utility>
 #include <vector>
 
 
@@ -33,3 +34,14 @@ using SupportedSignedArithmeticTypes = ::testing::Types<int8_t, int16_t, int32_t
 using SupportedIntegralTypes =
     ::testing::Types<int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t>;
 using SupportedFloatingPointTypes = ::testing::Types<float, double>;
+using SupportedArithmeticFloatingTypePair = ::testing::Types<
+    std::pair<float, int8_t>, std::pair<float, int16_t>, std::pair<float, int32_t>, std::pair<float, int64_t>,
+    std::pair<float, uint8_t>, std::pair<float, uint16_t>, std::pair<float, uint32_t>, std::pair<float, uint64_t>,
+    std::pair<float, double>, std::pair<float, float>, std::pair<double, int8_t>, std::pair<double, int16_t>,
+    std::pair<double, int32_t>, std::pair<double, int64_t>, std::pair<double, uint8_t>, std::pair<double, uint16_t>,
+    std::pair<double, uint32_t>, std::pair<double, uint64_t>, std::pair<double, double>, std::pair<double, float>>;
+using SupportedSignedArithmeticFloatingTypePair =
+    ::testing::Types<std::pair<float, int8_t>, std::pair<float, int16_t>, std::pair<float, int32_t>,
+                     std::pair<float, int64_t>, std::pair<float, double>, std::pair<float, float>,
+                     std::pair<double, int8_t>, std::pair<double, int16_t>, std::pair<double, int32_t>,
+                     std::pair<double, int64_t>, std::pair<double, double>, std::pair<double, float>>;
--- test-suite/src/math/matrix/matrix2x2/TransformationFactoryTests.cpp
+++ test-suite/src/math/matrix/matrix2x2/TransformationFactoryTests.cpp
@@ -4,10 +4,72 @@
  * @date Created on: April 17, 2026
  *
  * @brief Verify @ref fgm::Matrix2D transformation factory function (E.g: MakeRotation, MakeScale, etc.) logic.
- * 
+ *
  * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
  */
 
 
 #include "MatrixTestSetup.h"
+#include "utility"
 
+
+
+template <typename T>
+class Matrix2DRotation: public ::testing::Test
+{
+protected:
+    using FP_T = T::first_type;   // FP_T -> Floating-point Type
+    using COM_T = T::second_type; // COM_T -> Common Type
+
+    FP_T _angle;
+    fgm::Matrix2D<COM_T> _expectedMatrix;
+
+
+    void SetUp() override
+    {
+        _angle = fgm::constants::PI<FP_T> / FP_T(2.0);
+#ifdef FMG_LEFT_HANDED
+        _expectedMatrix = { fgm::Vector2D{ COM_T(0), COM_T(-1) }, fgm::Vector2D { COM_T(1), COM_T(0) } };
+#else
+        _expectedMatrix = { fgm::Vector2D{ COM_T(0), COM_T(1) }, fgm::Vector2D{ COM_T(-1), COM_T(0) } };
+#endif
+    }
+};
+/**
+ * @brief Test fixture for @ref fgm::Matrix2D rotation factory, parameterized
+ *        @ref SupportedSignedArithmeticFloatingTypePair
+ */
+TYPED_TEST_SUITE(Matrix2DRotation, SupportedSignedArithmeticFloatingTypePair);
+
+
+
+/**
+ * @addtogroup T_FGM_Mat2x2_Transforms
+ * @{
+ */
+
+/**************************************
+ *                                    *
+ *         COMPILE TIME TESTS         *
+ *                                    *
+ **************************************/
+namespace
+{
+    // TODO: Add tests
+}
+
+
+
+/**************************************
+ *                                    *
+ *           ROTATION TESTS           *
+ *                                    *
+ **************************************/
+
+/** @brief Verify that rotation transformation factory returns a rotation matrix. */
+TYPED_TEST(Matrix2DRotation, ReturnsExpectedRotationMatrix)
+{
+    EXPECT_MAT_EQ(this->_expectedMatrix, fgm::Matrix2D<typename TypeParam::first_type>::makeRotation(this->_angle));
+}
+
+/** @} */
