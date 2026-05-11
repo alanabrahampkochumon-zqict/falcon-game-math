2e56c387171225e7abc96af2b7e9d2019971af29
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-11 15:42:30 +0530

    docs: add doxygen group to SIMD

---
+++ simd/include/DoxygenGroups.h
@@ -0,0 +1,12 @@
+#pragma once
+/**
+ * @file DoxygenGroups.h
+ * @author Alan Abraham P Kochumon
+ * @date Created on: May 11, 2026
+ *
+ * @brief Doxygen grouping for categorizing SIMD operations.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+// TODO: Add groups
