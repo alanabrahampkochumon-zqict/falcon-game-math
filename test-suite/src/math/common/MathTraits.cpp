a7aa4a892bd6447950920382d9783f32d609ffbe
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-25 14:54:54 +0530

    test: add tests to verify Arithmetic concepts

--- test-suite/src/math/common/MathTraits.cpp
+++ test-suite/src/math/common/MathTraits.cpp
@@ -9,16 +9,108 @@
  */
 
 
+#include <array>
 #include <common/MathTraits.h>
 #include <cstdint>
+#include <vector>
 
 
 namespace
 {
+    /** @brief Verify that @ref fgm::Arithmetic concept constraints integrals and floating point types. */
+    namespace
+    {
+        static_assert(fgm::Arithmetic<uint8_t> == true);
+        static_assert(fgm::Arithmetic<uint16_t> == true);
+        static_assert(fgm::Arithmetic<uint32_t> == true);
+        static_assert(fgm::Arithmetic<uint64_t> == true);
+
+        static_assert(fgm::Arithmetic<int8_t> == true);
+        static_assert(fgm::Arithmetic<int16_t> == true);
+        static_assert(fgm::Arithmetic<int32_t> == true);
+        static_assert(fgm::Arithmetic<int64_t> == true);
+
+        static_assert(fgm::Arithmetic<float> == true);
+        static_assert(fgm::Arithmetic<double> == true);
+        static_assert(fgm::Arithmetic<bool> == true);
+
+        static_assert(fgm::Arithmetic<std::vector<int>> == false);
+        static_assert(fgm::Arithmetic<std::array<int, 5>> == false);
+
+    } // namespace
 
 
-    /** @brief Verify @ref fgm::StrictSignedness concept constraints two types of similar signedness. */
-    namespace 
+    /**
+     * @brief Verify that @ref fgm::SignedStrictArithmetic concept constraints signed integrals
+     *        and floating point types.
+     */
+    namespace
+    {
+        static_assert(fgm::SignedStrictArithmetic<uint8_t> == false);
+        static_assert(fgm::SignedStrictArithmetic<uint16_t> == false);
+        static_assert(fgm::SignedStrictArithmetic<uint32_t> == false);
+        static_assert(fgm::SignedStrictArithmetic<uint64_t> == false);
+        static_assert(fgm::SignedStrictArithmetic<bool> == false);
+
+        static_assert(fgm::SignedStrictArithmetic<int8_t> == true);
+        static_assert(fgm::SignedStrictArithmetic<int16_t> == true);
+        static_assert(fgm::SignedStrictArithmetic<int32_t> == true);
+        static_assert(fgm::SignedStrictArithmetic<int64_t> == true);
+
+        static_assert(fgm::SignedStrictArithmetic<float> == true);
+        static_assert(fgm::SignedStrictArithmetic<double> == true);
+    } // namespace
+
+
+    /**
+     * @brief Verify that @ref fgm::StrictArithmetic concept constraints all signed integrals
+     *        and floating point types except `bool`.
+     */
+    namespace
+    {
+        static_assert(fgm::StrictArithmetic<uint8_t> == true);
+        static_assert(fgm::StrictArithmetic<uint16_t> == true);
+        static_assert(fgm::StrictArithmetic<uint32_t> == true);
+        static_assert(fgm::StrictArithmetic<uint64_t> == true);
+
+        static_assert(fgm::StrictArithmetic<int8_t> == true);
+        static_assert(fgm::StrictArithmetic<int16_t> == true);
+        static_assert(fgm::StrictArithmetic<int32_t> == true);
+        static_assert(fgm::StrictArithmetic<int64_t> == true);
+
+        static_assert(fgm::StrictArithmetic<float> == true);
+        static_assert(fgm::StrictArithmetic<double> == true);
+
+        static_assert(fgm::StrictArithmetic<bool> == false);
+
+    } // namespace
+
+
+    /** @brief Verify that @ref fgm::WeakArithmetic concept constraints integrals and floating point types. */
+    namespace
+    {
+        static_assert(fgm::WeakArithmetic<uint8_t> == true);
+        static_assert(fgm::WeakArithmetic<uint16_t> == true);
+        static_assert(fgm::WeakArithmetic<uint32_t> == true);
+        static_assert(fgm::WeakArithmetic<uint64_t> == true);
+
+        static_assert(fgm::WeakArithmetic<int8_t> == true);
+        static_assert(fgm::WeakArithmetic<int16_t> == true);
+        static_assert(fgm::WeakArithmetic<int32_t> == true);
+        static_assert(fgm::WeakArithmetic<int64_t> == true);
+                           
+        static_assert(fgm::WeakArithmetic<float> == true);
+        static_assert(fgm::WeakArithmetic<double> == true);
+        static_assert(fgm::WeakArithmetic<bool> == true);
+                           
+        static_assert(fgm::WeakArithmetic<std::vector<int>> == false);
+        static_assert(fgm::WeakArithmetic<std::array<int, 5>> == false);
+
+    } // namespace
+
+
+    /** @brief Verify that @ref fgm::StrictSignedness concept constraints two types of similar signedness. */
+    namespace
     {
         // Unsigned types
         static_assert(fgm::StrictSignedness<uint8_t, uint8_t> == true);
@@ -91,6 +183,6 @@ namespace
         static_assert(fgm::StrictSignedness<uint16_t, int64_t> == false);
         static_assert(fgm::StrictSignedness<uint32_t, int64_t> == false);
         static_assert(fgm::StrictSignedness<uint64_t, int64_t> == false);
-    }
+    } // namespace
 
 } // namespace
