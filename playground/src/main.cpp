343342be0bd9174c047b0559846a7e15913e9328
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-06-08 08:21:38 +0530

    fix: update code to render triangle bounds

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -11,6 +11,7 @@
 
 #include <SDL3/SDL.h>
 #include <vectors/Vector2D.h>
+#include <algorithm>
 #include <array>
 #include <iostream>
 
@@ -43,42 +44,42 @@ static SDL_AppResult pollEvents(bool &runningState) {
 constexpr std::array vertices = {fgm::vec2{10.0f, 2.0f}, fgm::vec2{20.0f, 28.0f}, fgm::vec2{3.0f, 21.0f}};
 
 
-static void putPixel(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-    // Packed the colors into a single value
-    // uint32_t packedColor = SDL_MapRGBA(surface->format, nullptr, r, g, b, a);
-    // SDL_Palette* palette = SDL_GetSurfacePalette(surface);
-    uint32_t packedColor = SDL_MapSurfaceRGB(surface, r, g, b);
-    auto bytesPerPixel = SDL_BYTESPERPIXEL(surface->format);
-
-    // Cast row pointer to pixels to uint8_t
-    auto *pixelAddress = static_cast<uint8_t *>(surface->pixels)
-                               + (y * surface->pitch)
-                               + (x * bytesPerPixel);
-
-    switch (bytesPerPixel) {
-        case 1:
-            *pixelAddress = static_cast<uint8_t>(packedColor);
-            break;
-        case 2:
-            *reinterpret_cast<uint16_t*>(pixelAddress) = static_cast<uint16_t>(packedColor);
-            break;
-        case 3:
-            // 24-bit surfaces store RGB separately depending on system byte order
-            #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-                pixelAddress[0] = (packedColor >> 16) & 0xFF;
-                pixelAddress[1] = (packedColor >> 8) & 0xFF;
-                pixelAddress[2] = packedColor & 0xFF;
-            #else
-                pixelAddress[0] = packedColor & 0xFF;
-                pixelAddress[1] = (packedColor >> 8) & 0xFF;
-                pixelAddress[2] = (packedColor >> 16) & 0xFF;
-            #endif
-            break;
-        case 4:
-            *reinterpret_cast<uint32_t*>(pixelAddress) = packedColor;
-            break;
-    }
-}
+// static void putPixel(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
+//     // Packed the colors into a single value
+//     // uint32_t packedColor = SDL_MapRGBA(surface->format, nullptr, r, g, b, a);
+//     // SDL_Palette* palette = SDL_GetSurfacePalette(surface);
+//     uint32_t packedColor = SDL_MapSurfaceRGB(surface, r, g, b);
+//     auto bytesPerPixel = SDL_BYTESPERPIXEL(surface->format);
+//
+//     // Cast row pointer to pixels to uint8_t
+//     auto *pixelAddress = static_cast<uint8_t *>(surface->pixels)
+//                                + (y * surface->pitch)
+//                                + (x * bytesPerPixel);
+//
+//     switch (bytesPerPixel) {
+//         case 1:
+//             *pixelAddress = static_cast<uint8_t>(packedColor);
+//             break;
+//         case 2:
+//             *reinterpret_cast<uint16_t*>(pixelAddress) = static_cast<uint16_t>(packedColor);
+//             break;
+//         case 3:
+//             // 24-bit surfaces store RGB separately depending on system byte order
+//             #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+//                 pixelAddress[0] = (packedColor >> 16) & 0xFF;
+//                 pixelAddress[1] = (packedColor >> 8) & 0xFF;
+//                 pixelAddress[2] = packedColor & 0xFF;
+//             #else
+//                 pixelAddress[0] = packedColor & 0xFF;
+//                 pixelAddress[1] = (packedColor >> 8) & 0xFF;
+//                 pixelAddress[2] = (packedColor >> 16) & 0xFF;
+//             #endif
+//             break;
+//         case 4:
+//             *reinterpret_cast<uint32_t*>(pixelAddress) = packedColor;
+//             break;
+//     }
+// }
 
 int main() {
     // Setup the metadata for identifying the application
@@ -116,17 +117,21 @@ int main() {
         //      |    \ \/    |
         //      |     \/     |
         //      --------(maxX, maxY)
-        auto minX = static_cast<int>(std::min(vertices[0].x(), std::min(vertices[1].x(), vertices[2].x())));
-        auto minY = static_cast<int>(std::min(vertices[0].y(), std::min(vertices[1].y(), vertices[2].y())));
-        auto maxX = static_cast<int>(std::max(vertices[0].x(), std::max(vertices[1].x(), vertices[2].x())));
-        auto maxY = static_cast<int>(std::max(vertices[0].y(), std::max(vertices[1].y(), vertices[2].y())));
+        const auto minX = static_cast<int>(std::min({vertices[0].x(), vertices[1].x(), vertices[2].x()}));
+        const auto minY = static_cast<int>(std::min({vertices[0].y(), vertices[1].y(), vertices[2].y()}));
+        const auto maxX = static_cast<int>(std::max({vertices[0].x(), vertices[1].x(), vertices[2].x()}));
+        const auto maxY = static_cast<int>(std::max({vertices[0].y(), vertices[1].y(), vertices[2].y()}));
 
         SDL_Log("(minX, minY): (%d, %d)\n(maxX, maxY): (%d, %d)", minX, minY, maxX, maxY);
-        for (int i = 0; i < surface->w; ++i) {
-            for (int j = 0; j < surface->h; ++j) {
-                putPixel(surface, i, j, 0xF0, 0x0F, 0xFF);
+        for (int i = minX; i < maxX; ++i) {
+            for (int j = minY; j < maxY; ++j) {
+                // putPixel(surface, i, j, 0xF0, 0x0F, 0xFF);
+                // TODO: Replace with a faster function since this is slower
+                SDL_WriteSurfacePixel(surface, i, j, 0xf0, 0x0f, 0xff, 0xff);
             }
         }
+        // Updates the SDL surface by copying it to the screen
+        SDL_UpdateWindowSurface(window);
         // for (int i = minX; i < maxX; ++i) {
         //     for (int j = minY; j < maxY; ++j) {
         //         putPixel(surface, i, j, 0xFF, 0xFF, 0xFF);
