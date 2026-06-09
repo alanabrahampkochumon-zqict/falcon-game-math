3179e68676ae62c405d34b081130ce0520db0232
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-06-09 10:52:07 +0530

    refactor: move to buffer based sdl rendering

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -10,18 +10,21 @@
 
 
 #include <SDL3/SDL.h>
-#include <vectors/Vector2D.h>
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <vectors/Vector2D.h>
 
-static SDL_AppResult pollEvents(bool &runningState) {
+static SDL_AppResult pollEvents(bool& runningState)
+{
     // Event Queue
     SDL_Event event;
 
     // Event Loop
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
             case SDL_EVENT_MOUSE_MOTION:
                 // SDL_Log("We got a motion event.");
                 // SDL_Log("Current mouse position is: (%f, %f)", event.motion.x, event.motion.y);
@@ -41,18 +44,27 @@ static SDL_AppResult pollEvents(bool &runningState) {
 }
 
 
-class SoftwareRenderer {
+class SoftwareRenderer
+{
 public:
-    SoftwareRenderer(SDL_Window *window) : _window(window) {
+    SoftwareRenderer(SDL_Window* window): _window(window)
+    {
         _surface = SDL_GetWindowSurface(_window);
 
-        if (!_surface) {
+        if (!_surface)
+        {
             SDL_Log("Couldn't initialize a SDL surface: %s", SDL_GetError());
         }
+
+        const auto bufferSize =static_cast<std::size_t>(_surface->w * _surface->h * _colorChannels);
+        _buffer = new uint8_t[bufferSize];
+        memset(_buffer, 0, bufferSize);
     }
 
-    template<typename T>
-    void renderTriangle(const fgm::Vector2D<T> &v0, const fgm::Vector2D<T> &v1, const fgm::Vector2D<T> &v2, const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a) {
+    template <typename T>
+    void renderTriangle(const fgm::Vector2D<T>& v0, const fgm::Vector2D<T>& v1, const fgm::Vector2D<T>& v2,
+                        const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a)
+    {
         // Calculate the bounding box of the triangle
         // (minX, minY)--------
         //       |____________|
@@ -63,33 +75,49 @@ public:
         //       |    \ \/    |
         //       |     \/     |
         //       --------(maxX, maxY)
-        const auto minX = static_cast<int>(std::min({v0.x(), v1.x(), v2.x()}));
-        const auto minY = static_cast<int>(std::min({v0.y(), v1.y(), v2.y()}));
-        const auto maxX = static_cast<int>(std::max({v0.x(), v1.x(), v2.x()}));
-        const auto maxY = static_cast<int>(std::max({v0.y(), v1.y(), v2.y()}));
+        const auto minX = static_cast<int>(std::min({ v0.x(), v1.x(), v2.x() }));
+        const auto minY = static_cast<int>(std::min({ v0.y(), v1.y(), v2.y() }));
+        const auto maxX = static_cast<int>(std::max({ v0.x(), v1.x(), v2.x() }));
+        const auto maxY = static_cast<int>(std::max({ v0.y(), v1.y(), v2.y() }));
 
-        for (int i = minX; i < maxX; ++i) {
-            for (int j = minY; j < maxY; ++j) {
+        for (int i = minX; i < maxX; ++i)
+        {
+            for (int j = minY; j < maxY; ++j)
+            {
                 // putPixel(surface, i, j, 0xF0, 0x0F, 0xFF);
                 // TODO: Replace with a faster function since this is slower
-                const auto currentPoint = fgm::vec2(i, j);
+                const auto currentPoint = fgm::vec2(static_cast<T>(i), static_cast<T>(j));
                 if (insideTriangle(v0, v1, v2, currentPoint))
-                    SDL_WriteSurfacePixel(_surface, i, j, r, g, b, a);
+                {
+                    // SDL_WriteSurfacePixel(_surface, i, j, r, g, b, a); // NOTE: Left out for benchmarking
+                    const auto bufferOffset = _colorChannels * (j * _surface->w + j); // 4 -> r, g, b, a
+                    _buffer[bufferOffset + 0] = r;
+                    _buffer[bufferOffset + 1] = g;
+                    _buffer[bufferOffset + 2] = b;
+                    _buffer[bufferOffset + 3] = a;
+
+                }
             }
         }
+
+        // TODO: Separate out
+        SDL_Surface* srcSurface = SDL_CreateSurfaceFrom(_surface->w, _surface->h, _surface->format, _buffer, _surface->w * _colorChannels);
+
+        SDL_BlitSurface(srcSurface, nullptr, _surface, nullptr);
     }
 
 
 private:
-    template<typename T>
-    static bool insideTriangle(const fgm::Vector2D<T> &v0, const fgm::Vector2D<T> &v1, const fgm::Vector2D<T> &v2,
-                               const fgm::Vector2D<T> &point) {
+    template <typename T>
+    static bool insideTriangle(const fgm::Vector2D<T>& v0, const fgm::Vector2D<T>& v1, const fgm::Vector2D<T>& v2,
+                               const fgm::Vector2D<T>& point)
+    {
         // Edges
-        const auto e1 = v1 - v0;
+        const auto e1  = v1 - v0;
         const auto v1p = point - v1;
-        const auto e2 = v2 - v1;
+        const auto e2  = v2 - v1;
         const auto v2p = point - v2;
-        const auto e3 = v0 - v2;
+        const auto e3  = v0 - v2;
         const auto v0p = point - v0;
 
         // Return true if all point is bounded by all the three edges
@@ -97,29 +125,32 @@ private:
     }
 
     // Member variables
-    SDL_Window *_window;
-    SDL_Surface *_surface;
+    SDL_Window* _window;
+    SDL_Surface* _surface;
+    uint8_t* _buffer;
+    constexpr static int _colorChannels = 4;
 };
 
 // Note: Vertices are in screen space
 // 0 to width, 0 to height
-constexpr std::array VERTICES = {
-    fgm::vec2{10.0f, 2.0f}, fgm::vec2{240.0f, 300.0f}, fgm::vec2{10.0f, 580.0f}, fgm::vec2{700.0f, 550.0f}
-};
+constexpr std::array VERTICES = { fgm::vec2{ 10.0f, 2.0f }, fgm::vec2{ 240.0f, 300.0f }, fgm::vec2{ 10.0f, 580.0f },
+                                  fgm::vec2{ 700.0f, 550.0f } };
 
-int main() {
+int main()
+{
     // Set up the metadata for identifying the application
     SDL_SetAppMetadata("DEMO: FGM Software Rasterizer", "1.0.0", "com.fgm.demo.rasterizer");
 
 
     // Initialize the required SDL subsystems
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
     // Create the SDL Window
-    SDL_Window *window = SDL_CreateWindow("DEMO: FGM Software Rasterizer", 800, 600, 0);
+    SDL_Window* window = SDL_CreateWindow("DEMO: FGM Software Rasterizer", 800, 600, 0);
 
 
     // Initialize the state
@@ -127,7 +158,8 @@ int main() {
 
     bool runningState = true;
 
-    while (runningState) {
+    while (runningState)
+    {
         renderer.renderTriangle(VERTICES[0], VERTICES[1], VERTICES[2], 0xbc, 0x10, 0x20, 0xff);
         renderer.renderTriangle(VERTICES[1], VERTICES[3], VERTICES[2], 0x12, 0xcc, 0x20, 0xff);
 
