04fa69002ea0554e56d69dd6c0e3ca7a5b6e668d
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-07-21 14:32:12 +0530

    feat: add Mat4x3 string streaming to os

--- tests/fgm/matrices/mat4x3/StringRepresentationTests.cpp
+++ tests/fgm/matrices/mat4x3/StringRepresentationTests.cpp
@@ -3,13 +3,13 @@
  * @author Alan Abraham P Kochumon
  * @date Created on: July 21, 2026
  *
- * @brief Verify @ref fgm::Mat4x2 string representation.
+ * @brief Verify @ref fgm::Mat4x3 string representation.
  *
  * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
  */
 
 
-#include "Mat4x2TestSetup.h"
+#include "Mat4x3TestSetup.h"
 
 #include <sstream>
 
@@ -21,7 +21,7 @@
  *                                    *
  **************************************/
 
-class Mat4x2StringRepresentationTests: public ::testing::Test
+class Mat4x3StringRepresentationTests: public ::testing::Test
 {
     /** @brief Switch to use full precision for testing. */
     void SetUp() override { fgm::Config::useFullPrecision = true; }
@@ -38,14 +38,14 @@ class Mat4x2StringRepresentationTests: public ::testing::Test
  */
 
 /** @brief Verify that the string representation matches the expected format for integral types. */
-TEST_F(Mat4x2StringRepresentationTests, IntegralRepresentation)
+TEST_F(Mat4x3StringRepresentationTests, IntegralRepresentation)
 {
-    const fgm::Mat4x2 mat(1, 2, 3, 4, 5, 6, 7, 8);
+    const fgm::Mat4x3 mat(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
     std::stringstream ss;
 
     ss << mat;
 
-    EXPECT_EQ(ss.str(), "|1 2|\n|3 4|\n|5 6|\n|7 8|\n");
+    EXPECT_EQ(ss.str(), "|1 2 3|\n|4 5 6|\n|7 8 9|\n|10 11 12|\n");
 }
 
 
@@ -53,14 +53,14 @@ TEST_F(Mat4x2StringRepresentationTests, IntegralRepresentation)
  * @brief Verify that the string representation matches the expected format for single-precision
  *       floating-point types.
  */
-TEST_F(Mat4x2StringRepresentationTests, FloatRepresentation)
+TEST_F(Mat4x3StringRepresentationTests, FloatRepresentation)
 {
-    const fgm::Mat4x2 mat(1.2345f, 2.0f, 3.0f, 4.53823f, 3.323f, 2.003f, 1.5f, 3.25f);
+    const fgm::Mat4x3 mat(1.2345f, 2.0f, 3.0f, 4.53823f, 3.323f, 2.003f, 1.5f, 3.25f, 2.5f, 1.125f, 0.0f, 1.11111f);
     std::stringstream ss;
 
     ss << mat;
 
-    EXPECT_EQ(ss.str(), "|1.23450 2.00000|\n|3.00000 4.53823|\n|3.32300 2.00300|\n|1.50000 3.25000|\n");
+    EXPECT_EQ(ss.str(), "|1.23450 2.00000 3.00000|\n|4.53823 3.32300 2.00300|\n|1.50000 3.25000 2.50000|\n|1.12500 0.00000 1.11111|\n");
 }
 
 
@@ -68,18 +68,18 @@ TEST_F(Mat4x2StringRepresentationTests, FloatRepresentation)
  * @brief Verify that the string representation matches the expected format for double-precision
  *       floating-point types.
  */
-TEST_F(Mat4x2StringRepresentationTests, DoubleRepresentation)
+TEST_F(Mat4x3StringRepresentationTests, DoubleRepresentation)
 {
-    const fgm::Mat4x2 mat(1.2345789777, 2.65831, 5.39281, 2.0, 4.0, 2.0, 0.0, 5.75);
+    const fgm::Mat4x3 mat(1.2345789777, 2.65831, 5.39281, 2.0, 4.0, 2.0, 0.0, 5.75, 1.0, 2.0, 3.0, 1.125);
     std::stringstream ss;
 
     ss << mat;
 
     EXPECT_EQ(ss.str(),
-              "|1.234578977700000 2.658310000000000|\n"
-              "|5.392810000000000 2.000000000000000|\n"
-              "|4.000000000000000 2.000000000000000|\n"
-              "|0.000000000000000 5.750000000000000|\n");
+              "|1.234578977700000 2.658310000000000 5.392810000000000|\n"
+              "|2.000000000000000 4.000000000000000 2.000000000000000|\n"
+              "|0.000000000000000 5.750000000000000 1.000000000000000|\n"
+              "|2.000000000000000 3.000000000000000 1.125000000000000|\n");
 }
 
 /** @} */
