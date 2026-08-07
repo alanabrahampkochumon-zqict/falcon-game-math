3fce4e8e161564c0a6e35f46f1d7071f0c900b5a
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-08-07 14:44:03 +0530

    test: add Mat2x3 death tests to separate binary

--- tests/fgm/matrices/death-tests/Mat2x3.cpp
+++ tests/fgm/matrices/death-tests/Mat2x3.cpp
@@ -29,9 +29,7 @@ namespace
      **************************************/
 
     /**
-     * @brief Test fixture for @ref fgm::Mat2 (Row, Column) Access.
-     *
-     * @tparam T The numeric type (int, float, double...) for matrix values.
+     * @brief Test fixture for @ref fgm::Mat2x3 (Row, Column) Access.
      */
     class Mat2x3IndexingTests: public testing::TestWithParam<std::pair<std::size_t, std::size_t>>
     {};
@@ -42,9 +40,7 @@ namespace
 
 
     /**
-     * @brief Test fixture for @ref fgm::Mat2 Column Access.
-     *
-     * @tparam T The numeric type (int, float, double...) for matrix values.
+     * @brief Test fixture for @ref fgm::Mat2x3 Column Access.
      */
     class Mat2x3ColumnIndexingTests: public testing::TestWithParam<std::size_t>
     {};
@@ -58,7 +54,7 @@ namespace
      * @tparam T The numeric type (int, float, double...) for matrix values.
      */
     template <typename T>
-    class Mat2x3Division: public ::testing::Test
+    class Mat2x3Division: public testing::Test
     {
     protected:
         fgm::Mat2x3<T> _matrix;
@@ -76,46 +72,23 @@ namespace
     TYPED_TEST_SUITE(Mat2x3Division, SupportedArithmeticTypes);
 
 
-    /**
-     * @brief Test fixture for @ref fgm::Mat2x3 Division with NaN elements.
-     */
-    class NaNMat2x3Division: public ::testing::TestWithParam<fgm::Mat2x3<float>>
-    {};
-    INSTANTIATE_TEST_SUITE_P(Mat2x3DivisionTestSuite, NaNMat2x3Division,
-                             ::testing::Values(fgm::Mat2x3<float>(fgm::constants::NaN, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f),
-                                               fgm::Mat2x3<float>(3.0f, fgm::constants::NaN, 3.0f, 3.0f, 3.0f, 3.0f),
-                                               fgm::Mat2x3<float>(3.0f, 3.0f, fgm::constants::NaN, 3.0f, 3.0f, 3.0f),
-                                               fgm::Mat2x3<float>(3.0f, 3.0f, 3.0f, fgm::constants::NaN, 3.0f, 3.0f),
-                                               fgm::Mat2x3<float>(3.0f, 3.0f, 3.0f, 3.0f, fgm::constants::NaN, 3.0f),
-                                               fgm::Mat2x3<float>(3.0f, 3.0f, 3.0f, 3.0f, 3.0f, fgm::constants::NaN),
-                                               fgm::Mat2x3<float>(fgm ::constants::NaN, fgm::constants::NaN,
-                                                                  fgm ::constants::NaN, fgm ::constants::NaN,
-                                                                  fgm::constants::NaN, fgm::constants::NaN)));
+    // TODO: Add after adding nan assertion to /
+    // /**
+    //  * @brief Test fixture for @ref fgm::Mat2x3 Division with NaN elements.
+    //  */
+    // class NaNMat2x3Division: public testing::TestWithParam<fgm::Mat2x3<float>>
+    // {};
+    // INSTANTIATE_TEST_SUITE_P(Mat2x3DivisionTestSuite, NaNMat2x3Division,
+    //                          ::testing::Values(fgm::Mat2x3<float>(fgm::constants::NaN, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f),
+    //                                            fgm::Mat2x3<float>(3.0f, fgm::constants::NaN, 3.0f, 3.0f, 3.0f, 3.0f),
+    //                                            fgm::Mat2x3<float>(3.0f, 3.0f, fgm::constants::NaN, 3.0f, 3.0f, 3.0f),
+    //                                            fgm::Mat2x3<float>(3.0f, 3.0f, 3.0f, fgm::constants::NaN, 3.0f, 3.0f),
+    //                                            fgm::Mat2x3<float>(3.0f, 3.0f, 3.0f, 3.0f, fgm::constants::NaN, 3.0f),
+    //                                            fgm::Mat2x3<float>(3.0f, 3.0f, 3.0f, 3.0f, 3.0f, fgm::constants::NaN),
+    //                                            fgm::Mat2x3<float>(fgm ::constants::NaN, fgm::constants::NaN,
+    //                                                               fgm ::constants::NaN, fgm ::constants::NaN,
+    //                                                               fgm::constants::NaN, fgm::constants::NaN)));
 
-
-
-    /** @brief Test fixture for calculating @ref fgm::Mat2 inverse with singular matrices. */
-    class Mat2InverseSingularTests: public testing::TestWithParam<fgm::Mat2<float>>
-    {};
-    INSTANTIATE_TEST_SUITE_P(Mat2SingularMatrixInverse, Mat2InverseSingularTests,
-                             ::testing::Values(fgm::Mat2{ fgm::Vec2{ 1.0f, 2.0f }, fgm::Vec2{ 1.0f, 2.0f } },
-                                               fgm::Mat2{ fgm::Vec2{ 2.0f, 2.0f }, fgm::Vec2{ 2.0f, 2.0f } },
-                                               fgm::Mat2{ fgm::Vec2{ 3.0f, 2.0f }, fgm::Vec2{ 6.0f, 4.0f } },
-                                               fgm::Mat2{ fgm::Vec2{ 0.0f, 0.0f }, fgm::Vec2{ 4.0f, 5.0f } },
-                                               fgm::Mat2{ fgm::Vec2{ 0.0f, 3.0f }, fgm::Vec2{ 0.0f, 5.0f } }));
-
-
-
-    /** @brief Test fixture for @ref fgm::Mat2 inverse with NaN elements. */
-    class Mat2InverseNaNTests: public ::testing::TestWithParam<fgm::Mat2<float>>
-    {};
-    INSTANTIATE_TEST_SUITE_P(Mat2NaNMatrixInverse, Mat2InverseNaNTests,
-                             ::testing::Values(fgm::Mat2<float>(fgm::constants::NaN, 3.0f, 3.0f, 3.0f),
-                                               fgm::Mat2<float>(3.0f, fgm::constants::NaN, 3.0f, 3.0f),
-                                               fgm::Mat2<float>(3.0f, 3.0f, fgm::constants::NaN, 3.0f),
-                                               fgm::Mat2<float>(3.0f, 3.0f, 3.0f, fgm::constants::NaN),
-                                               fgm::Mat2<float>(fgm ::constants::NaN, fgm::constants::NaN,
-                                                                fgm ::constants::NaN, fgm ::constants::NaN)));
 } // namespace
 
 
@@ -161,35 +134,6 @@ TYPED_TEST(Mat2x3Division, DivideOperator_ByZeroTriggersAssertInDebugMode)
 
 TYPED_TEST(Mat2x3Division, DivideEqualsOperator_ByZeroTriggersAssertInDebugMode)
 { EXPECT_DEBUG_DEATH(this->_matrix /= 0, ""); }
-
-
-TEST_P(Mat2InverseSingularTests, Inverse_TriggersAssertionInDebugMode)
-{
-    const auto& matrix = GetParam();
-    // Static cast is placed to suppress the no-discard warning
-    EXPECT_DEBUG_DEATH(static_cast<void>(matrix.inverse()), "");
-}
-
-TEST_P(Mat2InverseSingularTests, StaticWrapper_Inverse_TriggersAssertionInDebugMode)
-{
-    const auto& matrix = GetParam();
-    // Static cast is placed to suppress the no-discard warning
-    EXPECT_DEBUG_DEATH(static_cast<void>(fgm::Mat2<float>::inverse(matrix)), "");
-}
-
-TEST_P(Mat2InverseNaNTests, Inverse_TriggersAssertionInDebugMode)
-{
-    const auto& matrix = GetParam();
-    // Static cast is placed to suppress the no-discard warning
-    EXPECT_DEBUG_DEATH(static_cast<void>(matrix.inverse()), "");
-}
-
-TEST_P(Mat2InverseNaNTests, StaticWrapper_Inverse_TriggersAssertionInDebugMode)
-{
-    const auto& matrix = GetParam();
-    // Static cast is placed to suppress the no-discard warning
-    EXPECT_DEBUG_DEATH(static_cast<void>(fgm::Mat2<float>::inverse(matrix)), "");
-}
 
 #endif
 
