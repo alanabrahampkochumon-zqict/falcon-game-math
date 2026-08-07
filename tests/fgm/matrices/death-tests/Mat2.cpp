0159c75c7c474053ee4ecf0319b2138a1c1dcd9a
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-08-07 13:37:48 +0530

    refactor: add matrix death test application. add mat2 death tests to it

--- tests/fgm/matrices/death-tests/Mat2.cpp
+++ tests/fgm/matrices/death-tests/Mat2.cpp
@@ -1,85 +1,126 @@
+/**
+ * @file Mat2.cpp
+ * @author Alan Abraham P Kochumon
+ * @date Created on: August 4, 2026
+ *
+ * @brief Verifies quaternion assertions in quaternion arithmetic operations that can result in application death.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
+#include "CommonSetup.h"
+#include "utils/MatrixUtils.h"
+#include "utils/VectorUtils.h"
+
+#include <fgm/matrices/Mat2.h>
+#include <utility>
 
 
 #ifdef ENABLE_DEBUG_TESTS
-    #include <utility>
+
+namespace
+{
+    /**************************************
+     *            TEST SETUP              *
+     **************************************/
 
     /**
      * @brief Test fixture for @ref fgm::Mat2 invalid (row, column) indices.
      */
     class Mat2IndexingTests: public testing::TestWithParam<std::pair<std::size_t, std::size_t>>
     {};
-INSTANTIATE_TEST_SUITE_P(Mat2OutOfBoundsRowColumnIndices, Mat2IndexingTests,
-                         testing::Values(std::make_pair(3, 3), std::make_pair(2, 3), std::make_pair(3, 2),
-                                         std::make_pair(100, 100)));
+    INSTANTIATE_TEST_SUITE_P(Mat2OutOfBoundsRowColumnIndices, Mat2IndexingTests,
+                             testing::Values(std::make_pair(3, 3), std::make_pair(2, 3), std::make_pair(3, 2),
+                                             std::make_pair(100, 100)));
 
 
-/**
- * @brief Test fixture for @ref fgm::Mat2 invalid column indexing.
- */
-class Mat2ColumnIndexingTests: public testing::TestWithParam<std::size_t>
-{};
-INSTANTIATE_TEST_SUITE_P(Mat2OutOfBoundsColumnIndices, Mat2ColumnIndexingTests, testing::Values(3, 4, 100));
-#endif
+    /**
+     * @brief Test fixture for @ref fgm::Mat2 Division.
+     *
+     * @tparam T The numeric type (int, float, double...) for matrix values.
+     */
+    template <typename T>
+    class Mat2DivisionTests: public testing::Test
+    {
+    protected:
+        fgm::Mat2<T> _matrix;
+        T _scalar;
+        fgm::Mat2<T> _expectedMatrix;
+
+        void SetUp() override
+        {
+            _matrix         = { fgm::Vec2<T>{ 7, 3 }, fgm::Vec2<T>{ 1, 6 } };
+            _scalar         = T(3);
+            _expectedMatrix = { fgm::Vec2{ T(2.333333333333333), T(1) }, fgm::Vec2{ T(0.3333333333333333), T(2) } };
+        }
+    };
+    TYPED_TEST_SUITE(Mat2DivisionTests, SupportedArithmeticTypes);
 
 
 
-/** @brief Test fixture for calculating @ref fgm::Mat2 inverse with singular matrices. */
-class Mat2InverseSingularTests: public testing::TestWithParam<fgm::Mat2<float>>
-{};
-INSTANTIATE_TEST_SUITE_P(Mat2SingularMatrixInverse, Mat2InverseSingularTests,
-                         ::testing::Values(fgm::Mat2{ fgm::Vec2{ 1.0f, 2.0f }, fgm::Vec2{ 1.0f, 2.0f } },
-                                           fgm::Mat2{ fgm::Vec2{ 2.0f, 2.0f }, fgm::Vec2{ 2.0f, 2.0f } },
-                                           fgm::Mat2{ fgm::Vec2{ 3.0f, 2.0f }, fgm::Vec2{ 6.0f, 4.0f } },
-                                           fgm::Mat2{ fgm::Vec2{ 0.0f, 0.0f }, fgm::Vec2{ 4.0f, 5.0f } },
-                                           fgm::Mat2{ fgm::Vec2{ 0.0f, 3.0f }, fgm::Vec2{ 0.0f, 5.0f } }));
+    /**
+     * @brief Test fixture for @ref fgm::Mat2 invalid column indexing.
+     */
+    class Mat2ColumnIndexingTests: public testing::TestWithParam<std::size_t>
+    {};
+    INSTANTIATE_TEST_SUITE_P(Mat2OutOfBoundsColumnIndices, Mat2ColumnIndexingTests, testing::Values(3, 4, 100));
 
 
 
-/** @brief Test fixture for @ref fgm::Mat2 inverse with NaN elements. */
-class Mat2InverseNaNTests: public ::testing::TestWithParam<fgm::Mat2<float>>
-{};
-INSTANTIATE_TEST_SUITE_P(Mat2NaNMatrixInverse, Mat2InverseNaNTests,
-                         ::testing::Values(fgm::Mat2<float>(fgm::constants::NaN, 3.0f, 3.0f, 3.0f),
-                                           fgm::Mat2<float>(3.0f, fgm::constants::NaN, 3.0f, 3.0f),
-                                           fgm::Mat2<float>(3.0f, 3.0f, fgm::constants::NaN, 3.0f),
-                                           fgm::Mat2<float>(3.0f, 3.0f, 3.0f, fgm::constants::NaN),
-                                           fgm::Mat2<float>(fgm ::constants::NaN, fgm::constants::NaN,
-                                                            fgm ::constants::NaN, fgm ::constants::NaN)));
+
+    /** @brief Test fixture for calculating @ref fgm::Mat2 inverse with singular matrices. */
+    class Mat2InverseSingularTests: public testing::TestWithParam<fgm::Mat2<float>>
+    {};
+    INSTANTIATE_TEST_SUITE_P(Mat2SingularMatrixInverse, Mat2InverseSingularTests,
+                             ::testing::Values(fgm::Mat2{ fgm::Vec2{ 1.0f, 2.0f }, fgm::Vec2{ 1.0f, 2.0f } },
+                                               fgm::Mat2{ fgm::Vec2{ 2.0f, 2.0f }, fgm::Vec2{ 2.0f, 2.0f } },
+                                               fgm::Mat2{ fgm::Vec2{ 3.0f, 2.0f }, fgm::Vec2{ 6.0f, 4.0f } },
+                                               fgm::Mat2{ fgm::Vec2{ 0.0f, 0.0f }, fgm::Vec2{ 4.0f, 5.0f } },
+                                               fgm::Mat2{ fgm::Vec2{ 0.0f, 3.0f }, fgm::Vec2{ 0.0f, 5.0f } }));
 
 
-#ifdef ENABLE_DEBUG_TESTS
 
-/** @brief Verify that @ref fgm::Mat2 out-of-bounds column access triggers assert in debug mode. */
-TEST_P(Mat2ColumnIndexingTests, OutOfBoundAccessTriggersAssertInDebugMode)
+    /** @brief Test fixture for @ref fgm::Mat2 inverse with NaN elements. */
+    class Mat2InverseNaNTests: public ::testing::TestWithParam<fgm::Mat2<float>>
+    {};
+    INSTANTIATE_TEST_SUITE_P(Mat2NaNMatrixInverse, Mat2InverseNaNTests,
+                             ::testing::Values(fgm::Mat2<float>(fgm::constants::NaN, 3.0f, 3.0f, 3.0f),
+                                               fgm::Mat2<float>(3.0f, fgm::constants::NaN, 3.0f, 3.0f),
+                                               fgm::Mat2<float>(3.0f, 3.0f, fgm::constants::NaN, 3.0f),
+                                               fgm::Mat2<float>(3.0f, 3.0f, 3.0f, fgm::constants::NaN),
+                                               fgm::Mat2<float>(fgm ::constants::NaN, fgm::constants::NaN,
+                                                                fgm ::constants::NaN, fgm ::constants::NaN)));
+} // namespace
+
+
+
+/**************************************
+ *           RUNTIME TESTS            *
+ **************************************/
+
+TEST_P(Mat2ColumnIndexingTests, OutOfBoundAccess_TriggersAssertInDebugMode)
 {
     const fgm::Mat2 mat(1, 2);
     const auto col = GetParam();
     EXPECT_DEBUG_DEATH(static_cast<void>(mat[col]), "");
 }
 
-/** @brief Verify that @ref fgm::Mat2 out-of-bounds row, column access triggers assert in debug mode. */
-TEST_P(Mat2IndexingTests, OutOfBoundAccessTriggersAssertInDebugMode)
+TEST_P(Mat2IndexingTests, OutOfBoundAccess_TriggersAssertInDebugMode)
 {
     const fgm::Mat2 mat(1, 2);
     const auto [row, col] = GetParam();
     EXPECT_DEBUG_DEATH(static_cast<void>(mat(row, col)), "");
 }
 
-#endif
 
 
-#ifdef ENABLE_DEBUG_TESTS
-
-/** @brief Verify that @ref fgm::Mat2 out-of-bounds column mutation triggers assert in debug mode. */
-TEST_P(Mat2ColumnIndexingTests, OutOfBoundMutationTriggersAssertInDebugMode)
+TEST_P(Mat2ColumnIndexingTests, OutOfBoundMutation_TriggersAssertInDebugMode)
 {
     [[maybe_unused]] fgm::Mat2 mat(1, 2);
     const auto col = GetParam();
     EXPECT_DEBUG_DEATH(static_cast<void>(mat[col] = fgm::Vec2<int>::zero()), "");
 }
 
-
-/** @brief Verify that @ref fgm::Mat2 out-of-bounds row, column mutation triggers assert in debug mode. */
 TEST_P(Mat2IndexingTests, OutOfBoundMutationTriggersAssertInDebugMode)
 {
     [[maybe_unused]] fgm::Mat2 mat(1, 2);
@@ -87,43 +128,38 @@ TEST_P(Mat2IndexingTests, OutOfBoundMutationTriggersAssertInDebugMode)
     EXPECT_DEBUG_DEATH(static_cast<void>(mat(row, col) = 5), "");
 }
 
-#endif
 
-#ifdef ENABLE_DEBUG_TESTS
 
-/** @brief Verify that assertion is triggered when dividing by zero (compound division) in **Debug Mode**. */
-TYPED_TEST(Mat2Division, DivideOperator_ByZeroTriggersAssertInDebugMode)
+TYPED_TEST(Mat2DivisionTests, DivideOperator_ByZeroTriggersAssertInDebugMode)
 { EXPECT_DEBUG_DEATH(static_cast<void>(this->_matrix / 0), ""); }
 
-#endif
-
-#ifdef ENABLE_DEBUG_TESTS
-
-/** @brief Verify that assertion is triggered when dividing by zero (compound division) in **Debug Mode**. */
-TYPED_TEST(Mat2Division, DivideEqualsOperator_ByZeroTriggersAssertInDebugMode)
+TYPED_TEST(Mat2DivisionTests, DivideEqualsOperator_ByZeroTriggersAssertInDebugMode)
 { EXPECT_DEBUG_DEATH(this->_matrix /= 0, ""); }
 
-#endif
 
 
-#ifdef ENABLE_DEBUG_TESTS
-
-/**
- * @brief Verify that inverting a singular matrix using @ref fgm::Mat2::inverse
- *        triggers assertion in debug mode.
- */
-TEST_P(Mat2InverseSingularTests, TriggersAssertionInDebugMode)
+TEST_P(Mat2InverseSingularTests, Inverse_TriggersAssertionInDebugMode)
 {
     const auto& matrix = GetParam();
     // Static cast is placed to suppress the no-discard warning
     EXPECT_DEBUG_DEATH(static_cast<void>(matrix.inverse()), "");
 }
 
-/**
- * @brief Verify that inverting a singular matrix using static variant of @ref fgm::Mat2::inverse
- *        triggers assertion in debug mode.
- */
-TEST_P(Mat2InverseSingularTests, StaticWrapper_TriggersAssertionInDebugMode)
+TEST_P(Mat2InverseSingularTests, StaticWrapper_Inverse_TriggersAssertionInDebugMode)
+{
+    const auto& matrix = GetParam();
+    // Static cast is placed to suppress the no-discard warning
+    EXPECT_DEBUG_DEATH(static_cast<void>(fgm::Mat2<float>::inverse(matrix)), "");
+}
+
+TEST_P(Mat2InverseNaNTests, Inverse_TriggersAssertionInDebugMode)
+{
+    const auto& matrix = GetParam();
+    // Static cast is placed to suppress the no-discard warning
+    EXPECT_DEBUG_DEATH(static_cast<void>(matrix.inverse()), "");
+}
+
+TEST_P(Mat2InverseNaNTests, StaticWrapper_Inverse_TriggersAssertionInDebugMode)
 {
     const auto& matrix = GetParam();
     // Static cast is placed to suppress the no-discard warning
