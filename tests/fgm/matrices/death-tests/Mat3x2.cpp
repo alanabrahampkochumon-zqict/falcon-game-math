4d6653c0f5a9f2f5e7a2596cb02ed1b6598e2e27
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-08-08 12:11:39 +0530

    refactor: add Mat3x2 death tests to separate binary

--- tests/fgm/matrices/death-tests/Mat3x2.cpp
+++ tests/fgm/matrices/death-tests/Mat3x2.cpp
@@ -26,7 +26,7 @@
 
 namespace
 {
-    fgm::Mat3x2 mat(1, 2, 3);
+    fgm::Mat3x2 mat(1, 2, 3, 4, 5, 6);
 
 
     /**************************************
@@ -43,9 +43,56 @@ namespace
                                              std::make_pair(100, 100)));
 
 
-    class Mat3x2ColumnIndexing: public testing::TestWithParam<std::size_t>
+
+    /**
+     * @brief Test fixture for @ref fgm::Mat3 invalid column indices.
+     */
+    class Mat3x2ColumnIndexingTests: public testing::TestWithParam<std::size_t>
     {};
-    INSTANTIATE_TEST_SUITE_P(Mat3x2OutOfBoundsColumnIndices, Mat3x2ColumnIndexing, testing::Values(3, 4, 100));
+    INSTANTIATE_TEST_SUITE_P(Mat3x2OutOfBoundsColumnIndices, Mat3x2ColumnIndexingTests, testing::Values(3, 4, 100));
+
+
+
+    /**
+     * @brief Test fixture for @ref fgm::Mat3x2 Division.
+     *
+     * @tparam T The numeric type (int, float, double...) for matrix values.
+     */
+    template <typename T>
+    class Mat3x2DivisionTests: public testing::Test
+    {
+    protected:
+        fgm::Mat3x2<T> _matrix;
+        T _scalar;
+        fgm::Mat3x2<T> _expectedMatrix;
+
+        void SetUp() override
+        {
+            _matrix         = { fgm::Vec3{ T(7), T(3), T(0) }, fgm::Vec3{ T(1), T(6), T(6) } };
+            _scalar         = T(3);
+            _expectedMatrix = { fgm::Vec3{ T(2.333333333333333), T(1), T(0) },
+                                fgm::Vec3{ T(0.3333333333333333), T(2), T(2) } };
+        }
+    };
+    /** Test fixture for @ref fgm::Mat3x2 division, parameterized by @ref SupportedArithmeticTypes */
+    TYPED_TEST_SUITE(Mat3x2DivisionTests, SupportedArithmeticTypes);
+
+
+
+    // TODO: Add checks for NaN Div
+    /** @brief Test fixture for @ref fgm::Mat3x2 division with NaN vectors. */
+    // class Mat3x2DivisionNaNTests: public testing::TestWithParam<fgm::Mat3x2<float>>
+    // {};
+    // INSTANTIATE_TEST_SUITE_P(Mat3x2InvalidDivision, Mat3x2DivisionNaNTests,
+    //                          ::testing::Values(fgm::Mat3x2<float>(fgm::constants::NaN, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f),
+    //                                            fgm::Mat3x2<float>(3.0f, fgm::constants::NaN, 3.0f, 3.0f, 3.0f, 3.0f),
+    //                                            fgm::Mat3x2<float>(3.0f, 3.0f, fgm::constants::NaN, 3.0f, 3.0f, 3.0f),
+    //                                            fgm::Mat3x2<float>(3.0f, 3.0f, 3.0f, fgm::constants::NaN, 3.0f, 3.0f),
+    //                                            fgm::Mat3x2<float>(3.0f, 3.0f, 3.0f, 3.0f, fgm::constants::NaN, 3.0f),
+    //                                            fgm::Mat3x2<float>(3.0f, 3.0f, 3.0f, 3.0f, 3.0f, fgm::constants::NaN),
+    //                                            fgm::Mat3x2<float>(fgm ::constants::NaN, fgm::constants::NaN,
+    //                                                               fgm ::constants::NaN, fgm ::constants::NaN,
+    //                                                               fgm::constants::NaN, fgm::constants::NaN)));
 
 
 } // namespace
@@ -83,13 +130,14 @@ TEST_P(Mat3x2IndexingTests, OutOfBoundMutationTriggersAssertInDebugMode)
     EXPECT_DEBUG_DEATH(static_cast<void>(mat(row, col) = 5), "");
 }
 
-/** @brief Verify that assertion is triggered when dividing by zero (compound division) in **Debug Mode**. */
+
 TYPED_TEST(Mat3x2DivisionTests, DivideOperator_ByZeroTriggersAssertInDebugMode)
 { EXPECT_DEBUG_DEATH(static_cast<void>(this->_matrix / 0), ""); }
 
-/** @brief Verify that assertion is triggered when dividing by zero (compound division) in **Debug Mode**. */
+
 TYPED_TEST(Mat3x2DivisionTests, DivideEqualsOperator_ByZeroTriggersAssertInDebugMode)
 { EXPECT_DEBUG_DEATH(this->_matrix /= 0, ""); }
+
 /** @} */
 
 #endif
